c459bce852c35c3f8493021501b2bc59d2ce499e
Benzazoua <billalben252@gmail.com>
2024-04-29 18:56:11 +0100

    Navbar component & style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navbar">
+      <ul className="navbar-list">
+        <li className="navbar-item">
+          <NavLink to="/" className="navbar-link" data-nav-link>
+            About
+          </NavLink>
+        </li>
 
-export default Navbar
+        <li className="navbar-item">
+          <NavLink to="/resume" className="navbar-link" data-nav-link>
+            Resume
+          </NavLink>
+        </li>
+
+        <li className="navbar-item">
+          <NavLink to="/portfolio" className="navbar-link" data-nav-link>
+            Portfolio
+          </NavLink>
+        </li>
+
+        <li className="navbar-item">
+          <NavLink to="/blogs" className="navbar-link" data-nav-link>
+            Blog
+          </NavLink>
+        </li>
+
+        <li className="navbar-item">
+          <NavLink to="/contact" className="navbar-link" data-nav-link>
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
