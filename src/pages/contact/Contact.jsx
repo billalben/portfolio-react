21d284b5196c07f50fc86dea7146d403aa998519
Benzazoua <billalben252@gmail.com>
2024-04-29 20:27:14 +0100

    Contact page

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,7 +1,62 @@
-import React from "react";
-
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section className="contact" data-page="contact">
+      <header>
+        <h2 className="h2 article-title">Contact</h2>
+      </header>
+
+      <section className="mapbox" data-mapbox>
+        <figure>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d104715.67059695667!2d-1.232758140380555!3d34.89729604601367!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd78c9b8f6b4ef59%3A0x576b9d7ddfa0780!2z2KrZhNmF2LPYp9mG!5e0!3m2!1sar!2sdz!4v1714418634294!5m2!1sar!2sdz"
+            width="400"
+            height="300"
+            loading="lazy"
+            title="Google Map"
+          ></iframe>
+        </figure>
+      </section>
+
+      <section className="contact-form">
+        <h3 className="h3 form-title">Contact Form</h3>
+
+        <form action="#" className="form" data-form>
+          <div className="input-wrapper">
+            <input
+              type="text"
+              name="full-name"
+              className="form-input"
+              placeholder="Full name"
+              required
+              data-form-input
+            />
+
+            <input
+              type="email"
+              name="email"
+              className="form-input"
+              placeholder="Email address"
+              required
+              data-form-input
+            />
+          </div>
+
+          <textarea
+            name="message"
+            className="form-input"
+            placeholder="Your Message"
+            required
+            data-form-input
+          ></textarea>
+
+          <button className="form-btn" type="submit" data-form-btn>
+            <ion-icon name="paper-plane"></ion-icon>
+            <span>Send Message</span>
+          </button>
+        </form>
+      </section>
+    </section>
+  );
 };
 
 export default Contact;
